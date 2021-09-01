e71e7736be12fbefa4115c81369d5e1c61d45dfb
WMYM <81931984+WashingtonYandun@users.noreply.github.com>
2021-08-31 20:57:58 -0500

    Solution
    list comprehension solution

--- multiples_of_3_5.py
+++ multiples_of_3_5.py
@@ -3,6 +3,11 @@ If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
 
 Find the sum of all the multiples of 3 or 5 below 1000.
 '''
+#list comprehension solution
+def solution(n):
+    return sum(num for num in range(1,n,1) if num%3==0 or num%5==0)
+
+
 #solution 1
 def solution(n):
     sum = 0
