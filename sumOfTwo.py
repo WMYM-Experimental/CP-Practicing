24489aa55a58cb15635e7860bf6dcff937d33c73
WMYM <81931984+WashingtonYandun@users.noreply.github.com>
2021-08-31 19:03:06 -0500

    Solution
    Solution

--- sumOfTwo.py
+++ sumOfTwo.py
@@ -1,3 +1,12 @@
 '''
 Given two lists of integers, is there a set of numbers — one from each list — whose sum is equal to a specified value
 '''
+
+def sumOfTwo(arrayA,arrayB,value):
+    band = False
+    for a in arrayA:
+        diff = value - a
+        if diff in arrayB:
+            band = True
+            return band
+    return band
